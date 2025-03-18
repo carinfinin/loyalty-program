cd3004034ffac050a0826f14ca18945ad31e4711
carinfinin <aleksejmelentev@gmail.com>
2025-03-19 01:29:27 +0300

    job

--- internal/service/service.go
+++ internal/service/service.go
@@ -57,7 +57,7 @@ func (s *Service) Worker() {
 func (s *Service) job(order *models.Order) {
 	const nf = "service job "
 
-	url := fmt.Sprintf("%s/api/orders/%d", s.Config.AccrualAddr, order.Number)
+	url := fmt.Sprintf("%s/api/orders/%v", s.Config.AccrualAddr, order.Number)
 	logger.Log.Info(nf, fmt.Sprintf("url: %v", url))
 
 	response, err := http.Get(url)
