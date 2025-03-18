e6060e95039f30bf3a8473e1054ae8ce00a1288e
carinfinin <aleksejmelentev@gmail.com>
2025-03-18 23:42:44 +0300

    add table

--- internal/store/models/models.go
+++ internal/store/models/models.go
@@ -4,8 +4,8 @@ import "time"
 
 type User struct {
 	ID       int64  `json:"-"`
-	Login    string `json:"login" validate:"required,min=3,max=10"`
-	Password string `json:"password" validate:"required,min=4,max=10"`
+	Login    string `json:"login" validate:"required,min=1,max=100"`
+	Password string `json:"password" validate:"required,min=1,max=100"`
 }
 
 type Order struct {
